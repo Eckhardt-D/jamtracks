289a7f7d23848344e088c73c3cb0572bdf38dd8e
ecki-d <support@kaizenmedia.co.za>
2019-09-17 20:45:19 +0200

    :rocket: remove extraneous files.

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <nuxt />
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ readme.md
@@ -0,0 +1,9 @@
+# JAMtracks
+
+A personal music manager built for a youtube tutorial.
+
+### Uses
+
+- Nuxt
+- Deezer API
+- Netlify
